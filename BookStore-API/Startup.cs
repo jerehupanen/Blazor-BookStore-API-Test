50d88c52c3b0b2fd6f1ea952a3487ddc23a287d6
Jere Hupanen <hupanenjere@gmail.com>
2021-11-07 18:46:37 +0200

    Added database classes and included Automapper nuget for mapping for DTO's. Fixed appsettings for the local database address.

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -1,5 +1,6 @@
 using BookStore_API.Contracts;
 using BookStore_API.Data;
+using BookStore_API.Mappings;
 using BookStore_API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -39,6 +40,8 @@ namespace BookStore_API
             services.AddCors(o => {
                 o.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(Maps));
 
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { 
